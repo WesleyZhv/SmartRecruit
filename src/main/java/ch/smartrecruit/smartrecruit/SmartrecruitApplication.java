1d1959271d411d70e99e2b65a3f6e57f31d24a1f
WesleyZhv <wesleyzhv@gmail.com>
2026-06-28 22:04:37 +0200

    setup : structure Spring Boot avec packages MVC + ML

---
+++ src/main/java/ch/smartrecruit/smartrecruit/SmartrecruitApplication.java
@@ -0,0 +1,13 @@
+package ch.smartrecruit.smartrecruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartrecruitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartrecruitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/smartrecruit/smartrecruit/SmartrecruitApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.smartrecruit.smartrecruit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartrecruitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
